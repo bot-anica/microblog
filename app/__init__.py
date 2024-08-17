3d891f7356aa5d2ae3b52e5fc3fb25c10a7548d8
Botanica <test.develop.user@gmail.com>
2024-08-17 09:23:46 +0300

    Complete register and login

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
 app.app_context().push()
-migrate = Migrate(app, db)
+
+db = SQLAlchemy(app)  # init SQLAlchemy for Flask-SQLAlchemy
+migrate = Migrate(app, db)  # init Migrate for Flask-Migrate
+
+login = LoginManager(app)  # init LoginManager for Flask-Login
+login.login_view = 'login'  # redirect to login page if user is not logged in
 
 from app import routes, models
-
