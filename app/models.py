06beb6c09022720dd0776ec4e77f07d6eee75876
Botanica <test.develop.user@gmail.com>
2024-08-20 12:48:30 +0300

    Add followers, add tests for User madel

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from hashlib import md5
-from sqlalchemy.sql import func
 
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy.sql import func
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db, login
 
@@ -13,6 +13,13 @@ def load_user(id):
     return User.query.get(int(id))
 
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -20,7 +27,15 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime, default=func.now)
+    last_seen = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    followed = db.relationship(
+        'User',
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic'
+    )
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -35,11 +50,31 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id
+        ).count() > 0
+
+    def followed_posts(self):
+        followed_posts = (Post.query
+                          .join(followers, (followers.c.followed_id == Post.user_id))
+                          .filter(followers.c.follower_id == self.id))
+        own_posts = Post.query.filter_by(user_id=self.id)
+        return followed_posts.union(own_posts).order_by(Post.timestamp.desc())
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=func.now)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now(timezone.utc))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
