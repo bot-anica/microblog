1373e41d275a6d67b9b28b934e022ed3e5c79b0d
Botanica <test.develop.user@gmail.com>
2024-08-22 23:54:49 +0300

    Add ability to reset password

--- app/forms.py
+++ app/forms.py
@@ -53,3 +53,16 @@ class PostForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_confirmation = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')]
+    )
+    submit = SubmitField('Request Password Reset')
+
+
--- app/models.py
+++ app/models.py
@@ -1,11 +1,12 @@
 from datetime import datetime, timezone
 from hashlib import md5
+from time import time
 
+import jwt
 from flask_login import UserMixin
-from sqlalchemy.sql import func
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from app import db, login
+from app import db, login, app
 
 
 def lazy_utc_now():
@@ -73,6 +74,22 @@ class User(UserMixin, db.Model):
                           .filter(followers.c.follower_id == self.id))
         own_posts = Post.query.filter_by(user_id=self.id)
         return followed_posts.union(own_posts).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'],
+            algorithm='HS256'
+        )
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+        except:
+            return
+
+        return User.query.get(id)
 
 
 class Post(db.Model):
