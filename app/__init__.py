30c42ba42e1739e187b5a9986561b2458723b516
Botanica <test.develop.user@gmail.com>
2024-08-25 23:47:34 +0300

    Display post timestamp and change user last seen format

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,23 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.app_context().push()
+
+db = SQLAlchemy(app)  # init SQLAlchemy for Flask-SQLAlchemy
+migrate = Migrate(app, db)  # init Migrate for Flask-Migrate
+
+login = LoginManager(app)  # init LoginManager for Flask-Login
+login.login_view = 'login'  # redirect to login page if user is not logged in
+
+mail = Mail(app)
+
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -50,13 +61,5 @@ if not app.debug:
     app.logger.addHandler(file_handler)
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
-
-db = SQLAlchemy(app)  # init SQLAlchemy for Flask-SQLAlchemy
-migrate = Migrate(app, db)  # init Migrate for Flask-Migrate
-
-login = LoginManager(app)  # init LoginManager for Flask-Login
-login.login_view = 'login'  # redirect to login page if user is not logged in
-
-mail = Mail(app)
 
 from app import routes, models, errors
